7939e509f895171c582a74c2a640a39ce32ea76f
junayet 701 <junayetahmed49@gmail.com>
2021-07-09 02:09:40 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    var typed = new Typed('.type', {
+        strings: ['designer'],
+        typeSpeed:200,
+        backSpeed:200,
+        smartBackspace: true,
+        loop:true,
+      });
+
+
+});
